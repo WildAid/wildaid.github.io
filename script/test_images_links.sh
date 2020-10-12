40f56f7c4465712916f8526cf45d98888f90a336
Sheeri <sheeri.cabral@mongodb.com>
2020-10-12 12:43:52 -0400

    Tweak ci (#97)
    * Fixes made while using the instructions to build charts.
    
    most notably - added some reusable sections and reused them.
    
    * Adding html proofer and a script to use it
    
    to help with #94
    
    * tweaking CI and adding ignore tags

--- script/test_images_links.sh
+++ script/test_images_links.sh
@@ -2,4 +2,4 @@
 set -euo pipefail # halt script on error
 
 bundle exec jekyll build
-bundle exec htmlproofer ./_site
+bundle exec htmlproofer ./_site --allow-hash-href  --empty-alt-ignore --check_img_http 
